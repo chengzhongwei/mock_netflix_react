a8ab843d8e0a89035742e2c6344b97d4fcd75975
Chengzhong Wei <chengzhongpr@gmail.com>
2021-03-29 18:53:21 -0700

    moved layout.js out of /component folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 
-import Layout from "./components/Layout"
+import Layout from "./Layout"
 import store from "./store"
 
 const app = document.getElementById('root')
